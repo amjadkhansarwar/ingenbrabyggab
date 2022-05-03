9fb5e8a6186fc17efb77ed00f81dfc41057355f6
amjadkhansarwar <muhammad.amjad@iths.se>
2022-05-03 14:43:58 +0200

    CURD Opration
    now our Curd opreation is working

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,31 @@
 const User = require('../models/User')
 
-module.exports = {
-  
-    CreateUser:  async (req, res)=>{
+class Users {
+    static async CreateUser(req, res) {
         const {name,email,password} = req.body
         const user =await User.create({name, email, password})
       res.json({user})
-    },
-  }
+    }
+    static async UpdateUser(req, res) {
+        const id = req.params.id
+        const {name,email,password} = req.body
+        const user =await User.updateOne({_id: id},{name: name,email:email,password: password})
+      res.json({user})
+    }
+    static async GetOneUser(req, res) {
+        const id = req.params.id
+        const user =await User.findOne({_id: id})
+      res.json({user})
+    }
+    static async GetAllUsers(req, res) {
+        const users = await User.find({})
+      res.json({users})
+    }
+    static async DeleteUser(req, res) {
+        const {email} = req.body
+        const user =await User.deleteOne({email: email})
+      res.json({user})
+    }
+}
+
+module.exports = Users
