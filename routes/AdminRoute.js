accd2211e698fb6dc2c076ae77b843ba48be42f2
amjadkhansarwar <muhammad.amjad@iths.se>
2022-05-09 09:26:54 +0200

    Web-Sockiets

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -5,21 +5,10 @@ const TaskController = require('../controllers/TaskController')
 const auth = require('../middlewares/auth')
 const useerValidation = require('../validator/validate')
 
-//----------Admin----------------//
-
 router.get('/loginadmin', useerValidation.loginValidate, adminController.loginAdmin)
-
-//------------worker----------------------//
-
 router.post('/createworker',  useerValidation.createValidate, auth.adminAuth, adminController.CreateWorker)
 
 router.delete('/deleteworker/:id', auth.adminAuth, adminController.DeleteWorker)
-
-//router.patch('/updateworker/:id', auth.adminAuth, adminController.UpdateWorker)
-//router.get('/getallworker', auth.adminAuth, adminController.GetAllWorker)
-//router.get('/getoneworker/:id', auth.adminAuth, adminController.GetOneWorker)
-
-//------------Client----------------------//
 
 router.post('/createclient',  useerValidation.createValidate, auth.adminAuth, adminController.CreateClient)
 
